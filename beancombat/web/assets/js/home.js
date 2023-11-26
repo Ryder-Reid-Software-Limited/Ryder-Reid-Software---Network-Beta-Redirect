f78b3979a469e7f8e5afc5c42d8259d3dbccc3bf
ryderreid <shadowjreid@gmail.com>
2023-11-26 19:59:59 +1100

    MFI BC

---
+++ beancombat/web/assets/js/home.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let progress = 0;
+    const loadingBar = document.getElementById("loading-bar");
+
+    function simulateLoading() {
+        progress += 10;
+        loadingBar.style.width = progress + "%";
+
+        if (progress < 100) {
+            setTimeout(simulateLoading, 1000);
+        } else {
+            document.getElementById("loading-screen").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        }
+    }
+
+    simulateLoading();
+});
